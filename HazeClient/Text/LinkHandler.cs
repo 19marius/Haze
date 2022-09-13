a9b172bc5dfafd00115d67bfc8898dd8d59530e3
19marius <chiricutamarius78@gmail.com>
2022-09-13 20:49:11 +0300

    changed all traces of 'messenger' to 'haze'

---
+++ HazeClient/Text/LinkHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HazeClient.Text
+{
+    /// <summary>
+    /// Represents a handler for links in <see cref="FormattedText"/> objects.
+    /// </summary>
+    public class LinkHandler
+    {
+        /// <summary>
+        /// Gets the index of the first character of the text over which this effect group is applied to in the unformatted string.
+        /// </summary>
+        public int Index { get; }
+
+        /// <summary>
+        /// Gets the the text over which this link is applied to.
+        /// </summary>
+        public string Text { get; }
+
+        /// <summary>
+        /// Gets the effect which will be applied when clicking on the affected text of the Owner object.
+        /// </summary>
+        public FormattedTextEffect ClickEffect { get; }
+
+        /// <summary>
+        /// Gets the action this <see cref="LinkHandler"/> refers to.
+        /// </summary>
+        public Action Handler { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating if this <see cref="LinkHandler"/> is active. That is, if the click effect and handler execute.
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkHandler"/> class from an owner and the speicfied action the link refers to.
+        /// </summary>
+        public LinkHandler(FormattedTextEffect effect, string text, int index, Action handler)
+        {
+            Text = text;
+            Index = index;
+            Handler = handler;
+            ClickEffect = effect;
+        }
+    }
+}
---
+++ HazeClient/Users/LoginSession.cs
@@ -0,0 +1,90 @@
+﻿using System.Net.Mail;
+
+namespace HazeClient.Users
+{
+    /// <summary>
+    /// Represents a log in session.
+    /// </summary>
+    public class LoginSession
+    {
+        #region Delegates
+
+        /// <summary>
+        /// Represents the method(s) that will be called whenever a password confirmation fails.
+        /// </summary>
+        public delegate void PasswordConfimationFailEventHandler(object sender, PasswordConfimationFailEventArgs e);
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the address used by this session.
+        /// </summary>
+        public MailAddress Address { get; set; }
+
+        /// <summary>
+        /// Gets the password used by this session.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating wether this session requires signing in.
+        /// </summary>
+        public bool SignInRequired { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating wether this session will make the user's account persistent across multiple sessions.
+        /// </summary>
+        public bool ShouldPersist { get; set; }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Occurs when a password confirmation fails.
+        /// </summary>
+        public event PasswordConfimationFailEventHandler PasswordConfimationFail;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginSession"/> class.
+        /// </summary>
+        public LoginSession()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginSession"/> class, utilizing an address and a password.
+        /// </summary>
+        public LoginSession(MailAddress address, string password)
+        {
+            Address = address;
+            Password = password;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginSession"/> class, utilizing an address and a password, and specifying if this session requires signing in.
+        /// </summary>
+        public LoginSession(MailAddress address, string password, bool signInRequired)
+        {
+            Address = address;
+            Password = password;
+            SignInRequired = signInRequired;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Confirms a password against the initial password. If not equal, invokes the PasswordConfirmationFail event.
+        /// </summary>
+        public void ConfirmPassword(string confirmedPassword)
+        {
+            if (SignInRequired && !confirmedPassword.Equals(Password)) PasswordConfimationFail?.Invoke(this, new PasswordConfimationFailEventArgs(Password, confirmedPassword));
+        }
+    }
+}
